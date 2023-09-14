18704649998945910f3a108289ea6ad4b19c705d
Simran Mandal <simranmandal0211@gmail.com>
2023-09-14 22:16:32 +0530

    Update:: DB connecting code

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,21 +1,20 @@
 require('dotenv').config()
 const mongoose = require('mongoose');
 
-exports.connectMongoose = () => {
-  mongoose.connect(process.env.DB_URL,{
-    useNewUrlParser: true
-  })
-  .then((e) => console.log('Connected to MongoDB -> CSV Upload'))
-  .catch((e) => console.log("Not Connect MongoDB"))
+// exports.connectMongoose = () => {
+//   mongoose.connect(process.env.DB_URL,{
+//     useNewUrlParser: true
+//   })
+//   .then((e) => console.log('Connected to MongoDB -> CSV Upload'))
+//   .catch((e) => console.log("Not Connect MongoDB"))
+// }
+
+
+module.exports.connectMongoose = async () => {
+  try{
+    await mongoose.connect(process.env.DB_URL)
+    console.log('connect to MongoDB sucessfully')
+  }catch(error){
+    console.timeLog('connect failed' +error.message)
+  }
 }
-
-
-// mongoose.connect(`mongodb://0.0.0.0/${process.env.DB}`);
-
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'error in connecting to the db'));
-// db.once('open', function(){
-//   console.log('Successfully connected to the DB');
-// });
-
-// module.exports = db;
